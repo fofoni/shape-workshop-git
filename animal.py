d0c108ab5b01c1e8c1694459ace20c86086fb5a5
ACbarros <anna.barros20@gmail.com>
2022-08-19 17:24:38 -0300

    topeira

--- animal.py
+++ animal.py
@@ -2,16 +2,9 @@
 Classes de animais
 """
 
-import pandas as pd
-import numpy  as np
 
-class Cachorro(self):
-    
-    def __init__ (self, name):
-        self.name = name
-    
-    def latir(self):
-        print('Au!Au!')
-        
-    def nome(self):
-        print(f'{self.name}')
+class topeira:
+    def __init__(self):
+        self.nome = "ruffus"
+        self.funcao = "salvar o Ron"
+        self.desenho = "Kim Possible"
